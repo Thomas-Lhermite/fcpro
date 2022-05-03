b5b4882e59ac9b48518a3c751385044664ad12f6
LHERMITE Thomas <lhermite.th14@gmail.com>
2022-05-03 17:26:20 +0200

    Add debug infos

--- login/config.php
+++ login/config.php
@@ -1,7 +1,10 @@
 <?php
+print("1");
     try {
         $bdd = new PDO('mysql:host=localhost;dbname=n-d-l-p-fcpro;charset=utf8', '266745_admin', 'Admin12345@');
+        print("2");
     } catch (Exception $e) {
+        print("3");
         die('Erreur'.$e->getMessage());
     }
 
--- login/connexion.php
+++ login/connexion.php
@@ -2,11 +2,13 @@
     session_start();
     require_once 'config.php';
 
+    print("4");
+
     if(isset($_POST['username']) && isset($_POST['password']))
     {
         $user = htmlspecialchars($_POST['username']); 
         $password = htmlspecialchars($_POST['password']); 
-        
+        print("5");
         $check = $bdd->prepare('SELECT id, username, password, permissions FROM users WHERE username = ?');
         $check->execute(array($user));
         $data = $check->fetch();
@@ -16,21 +18,26 @@
 
         if($row > 0)
         {
+            print("6");
             $password_hash = hash('sha256', $password);
             if($data['password'] === $password_hash)
             {
+                print("7");
                 $_SESSION['user'] = $data['username'];
                 $_SESSION['permissions'] = $data['permissions'];
                 $_SESSION['id'] = $data['id'];
                 header('Location: ../index.php');
                 die();
             }else{ 
-                header('Location: index.php?login_err=password'); die(); 
+                print("8");
+                // header('Location: index.php?login_err=password'); die(); 
             }
         }else{ 
-            header('Location: index.php?login_err=already'); die(); 
+            print("9");
+            // header('Location: index.php?login_err=already'); die(); 
         }
     }else{ 
-        header('Location: index.php'); die();
+        print("10");
+        // header('Location: index.php'); die();
     }
 ?>
