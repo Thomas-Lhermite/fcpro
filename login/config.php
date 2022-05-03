5f589430cacde9099ae02454563a7c494a232ad7
LHERMITE Thomas <lhermite.th14@gmail.com>
2022-05-03 17:29:59 +0200

    Change database name

--- login/config.php
+++ login/config.php
@@ -1,7 +1,7 @@
 <?php
 print("1");
     try {
-        $bdd = new PDO('mysql:host=mysql-n-d-l-p-fcpro.alwaysdata.net;dbname=n-d-l-p-fcpro;charset=utf8', '266745_admin', 'Admin12345@');
+        $bdd = new PDO('mysql:host=mysql-n-d-l-p-fcpro.alwaysdata.net;dbname=n-d-l-p-fcpro_bd;charset=utf8', '266745_admin', 'Admin12345@');
         print("2");
     } catch (Exception $e) {
         print("3");
